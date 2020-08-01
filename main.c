528bc34481212ddc409142ee1aa83078ef685126
Steven Impens <s.impens.dev@gmail.com>
2020-08-01 21:21:35 +0200

    Static lights

--- main.c
+++ main.c
@@ -1,23 +1,36 @@
 #include "board.h"
 #include "hal.h"
 #include "ch.h"
-#include "string.h"
 #include "ap2_qmk_led.h"
 #include "light_utils.h"
 
+/*
+ * Function declarations
+ */
 static void columnCallback(GPTDriver* driver);
-static void animationCallback(GPTDriver* driver);
+// static void animationCallback(GPTDriver* driver);
+inline void sPWM(uint8_t cycle, uint8_t currentCount, uint8_t start, ioline_t port);
+static void executeMsg(msg_t msg);
+static void disableLeds(void);
 
-void executeMsg(msg_t msg);
-void switchProfile(void);
-void rainbowHorizontal(void);
-void rainbowVertical(void);
-void miamiNights(void);
-void animatedRainbow(void);
-void disableLeds(void);
-void ledSet(void);
-void ledRowSet(void);
+#define REFRESH_FREQUENCY   200
 
+/*
+ * Serial configuration
+ */
+static const SerialConfig usart1Config = {
+    .speed = 115200
+};
+
+/*
+ * Column multiplex configurations
+ */
+static const GPTConfig bftm0Config = {
+    .frequency = NUM_COLUMN * REFRESH_FREQUENCY * 2 * 16,
+    .callback = columnCallback
+};
+
+led_t currentKeyLedColors[70];
 ioline_t ledColumns[NUM_COLUMN] = {
     LINE_LED_COL_1, 
     LINE_LED_COL_2, 
@@ -53,48 +66,6 @@ ioline_t ledRows[NUM_ROW * 3] = {
     LINE_LED_ROW_5_B,
 };
 
-#define REFRESH_FREQUENCY           200
-#define ANIMATION_TIMER_FREQUENCY   60
-
-#define LEN(a) (sizeof(a)/sizeof(*a))
-
-/*
-* Full keyboard static profiles
-* Each entry is a color in HEX
-*/
-static const uint32_t colorPalette[] = {0x9c0000, 0x9c9900, 0x1f9c00, 0x00979c};
-
-/*
-* Amount of profiles
-* fullStaticProfiles + Custom profiles
-*/
-static const uint16_t NUM_LIGHTING_PROFILES = LEN(colorPalette) + 4;
-
-// Indicates the ID of the current lighting profile
-static uint8_t lightingProfile = 0;
-
-led_t ledColors[70];
-static uint32_t currentColumn = 0;
-static uint32_t columnPWMCount = 0;
-
-// BFTM0 Configuration, this runs at 15 * REFRESH_FREQUENCY Hz
-static const GPTConfig bftm0Config = {
-    .frequency = NUM_COLUMN * REFRESH_FREQUENCY * 2 * 16,
-    .callback = columnCallback
-};
-
-// Lighting animation refresh timer
-static const GPTConfig lightAnimationConfig = {
-    .frequency = ANIMATION_TIMER_FREQUENCY,
-    .callback = animationCallback
-};
-
-static const SerialConfig usart1Config = {
-    .speed = 115200
-};
-
-static uint8_t commandBuffer[64];
-
 /*
  * Thread 1.
  */
@@ -110,191 +81,89 @@ THD_FUNCTION(Thread1, arg) {
     }
 }
 
-void executeMsg(msg_t msg){
+static void executeMsg(msg_t msg){
     switch (msg) {
         case CMD_LED_ON:
-            switchProfile();
+            setAllKeysColor(&currentKeyLedColors, 0x00979c);
             break;
         case CMD_LED_OFF:
             disableLeds();
             break;
         case CMD_LED_SET:
-            ledSet();
+            
             break;
         case CMD_LED_SET_ROW:
-            ledRowSet();
+            
             break;
         default:
             break;
     }
 }
 
-void switchProfile(){
-    chSysLock();
-
-    switch(lightingProfile){
-        case LEN(colorPalette):
-            rainbowHorizontal();
-            break;
-        case LEN(colorPalette) + 1:
-            rainbowVertical();
-            break;
-        case LEN(colorPalette) + 2:
-            miamiNights();
-            break;
-        case LEN(colorPalette) + 3:
-            animatedRainbow();
-            break;
-        default:
-              setAllKeysColor(ledColors, colorPalette[lightingProfile]);
-    }
-
-    palSetLine(LINE_LED_PWR);
-    lightingProfile = (lightingProfile+1)%NUM_LIGHTING_PROFILES;
-    chSysUnlock();
-}
-
-void rainbowHorizontal(){
-    for (uint16_t i=0; i<NUM_ROW; ++i){
-        for (uint16_t j=0; j<NUM_COLUMN; ++j){
-            setKeyColor(&ledColors[i*NUM_COLUMN+j], colorPalette[i%LEN(colorPalette)]);
-        }     
-    }
-}
-
-void rainbowVertical(){
-    for (uint16_t i=0; i<NUM_COLUMN; ++i){
-        for (uint16_t j=0; j<NUM_ROW; ++j){
-            setKeyColor(&ledColors[j*NUM_COLUMN+i], colorPalette[i%LEN(colorPalette)]);
-        }     
-    }
-}
-
-void miamiNights(){
-    setAllKeysColor(ledColors, 0x00979c);
-    setModKeysColor(ledColors, 0x9c008f);
-}
-
-void animatedRainbow(){
-    for (uint16_t i=0; i<NUM_COLUMN; ++i){
-        for (uint16_t j=0; j<NUM_ROW; ++j){
-            setKeyColor(&ledColors[j*NUM_COLUMN+i], colorPalette[i%LEN(colorPalette)]);
-        }
-    }
-}
-
-void disableLeds(){
-    lightingProfile = (lightingProfile+LEN(colorPalette)-1)%LEN(colorPalette);
+static void disableLeds(){
     palClearLine(LINE_LED_PWR);
 }
 
-void ledSet(){
-    size_t bytesRead;
-    bytesRead = sdReadTimeout(&SD1, commandBuffer, 4, 10000);
-    if (bytesRead >= 4){
-        if (commandBuffer[0] < NUM_ROW || commandBuffer[1] < NUM_COLUMN){
-            setKeyColor(&ledColors[commandBuffer[0] * NUM_COLUMN + commandBuffer[1]], ((uint16_t)commandBuffer[3] << 8 | commandBuffer[2]));
+
+static uint32_t currentColumn = 0;
+static uint32_t columnPWMCount = 0;
+
+void columnCallback(GPTDriver* _driver){
+    (void)_driver;
+    palClearLine(ledColumns[currentColumn]);
+    currentColumn = (currentColumn+1) % NUM_COLUMN;
+    palSetLine(ledColumns[currentColumn]);
+
+    if (columnPWMCount < 255){
+        for (size_t row = 0; row < NUM_ROW; row++){
+            const led_t keyLED = currentKeyLedColors[currentColumn + (NUM_COLUMN * row)];
+            const uint8_t red = keyLED.red;
+            const uint8_t green = keyLED.green;
+            const uint8_t blue = keyLED.blue;
+
+            sPWM(red, columnPWMCount, 0, ledRows[row * 3]);
+            sPWM(green, columnPWMCount, red, ledRows[row * 3+1]);
+            sPWM(blue, columnPWMCount, red+green, ledRows[row * 3+2]);
         }
+        columnPWMCount++;
+    }else{
+        columnPWMCount = 0;
     }
-}
-
-void ledRowSet(){
-    size_t bytesRead;
-    bytesRead = sdReadTimeout(&SD1, commandBuffer, sizeof(uint16_t) * NUM_COLUMN + 1, 1000);
-    if (bytesRead >= sizeof(uint16_t) * NUM_COLUMN + 1){
-        if (commandBuffer[0] < NUM_ROW){
-            memcpy(&ledColors[commandBuffer[0] * NUM_COLUMN],&commandBuffer[1], sizeof(uint16_t) * NUM_COLUMN);
-        }
-    }
-}
-
-inline uint8_t min(uint8_t a, uint8_t b){
-  return a<=b?a:b;
-}
-
-// Column offset for rainbow animation
-static uint8_t colAnimOffset = 0;
-
-// Update lighting table as per animation
-void animationCallback(GPTDriver* _driver){
-  
-  // Update lighting according to the current lighting profile
-  switch(lightingProfile){
-    
-    // Vertical Rainbow Profile
-    case 0:
-      // Set refresh rate for this animation
-      gptChangeInterval(_driver, ANIMATION_TIMER_FREQUENCY/5);
-      // Update led colors
-      for (uint16_t i=0; i<NUM_COLUMN; ++i){
-        for (uint16_t j=0; j<NUM_ROW; ++j){
-          setKeyColor(&ledColors[j*NUM_COLUMN+i], colorPalette[(i + colAnimOffset)%LEN(colorPalette)]);
-        }     
-      }
-      colAnimOffset = (colAnimOffset + 1)%LEN(colorPalette);
-      break;
-
-  }
 }
 
 inline void sPWM(uint8_t cycle, uint8_t currentCount, uint8_t start, ioline_t port){
-  if (start+cycle>0xFF) start = 0xFF - cycle;
-  if (start <= currentCount && currentCount < start+cycle)
-    palSetLine(port);
-  else
-    palClearLine(port);
-}
-
-void columnCallback(GPTDriver* _driver)
-{
-  (void)_driver;
-  palClearLine(ledColumns[currentColumn]);
-  currentColumn = (currentColumn+1) % NUM_COLUMN;
-  palSetLine(ledColumns[currentColumn]);
-  if (columnPWMCount < 255)
-  {
-    for (size_t row = 0; row < NUM_ROW; row++)
-    {
-    const led_t keyLED = ledColors[currentColumn + (NUM_COLUMN * row)];
-    const uint8_t red = keyLED.red;
-    const uint8_t green = keyLED.green;
-    const uint8_t blue = keyLED.blue;
-
-    sPWM(red, columnPWMCount, 0, ledRows[row * 3]);
-    sPWM(green, columnPWMCount, red, ledRows[row * 3+1]);
-    sPWM(blue, columnPWMCount, red+green, ledRows[row * 3+2]);
+    if (start+cycle>0xFF) start = 0xFF - cycle;
+    if (start <= currentCount && currentCount < start+cycle){
+        palSetLine(port);
+    }else{
+        palClearLine(port);
     }
-    columnPWMCount++;
-  }
-  else
-  {
-    columnPWMCount = 0;
-  }
 }
+
 
 /*
  * Application entry point.
  * HAL initialization en setup
- * Low priotiy thread
+ * Create Thread 1
  */
 int main(void) {
-  halInit();
-  chSysInit();
+    halInit();
+    chSysInit();
 
-  sdStart(&SD1, &usart1Config);
-  palSetLine(LINE_LED_PWR);
+    sdStart(&SD1, &usart1Config);
+    palSetLine(LINE_LED_PWR);
 
-  // Setup Column Multiplex Timer
-  gptStart(&GPTD_BFTM0, &bftm0Config);
-  gptStartContinuous(&GPTD_BFTM0, 1);
+    // Setup Column Multiplex Timer
+    gptStart(&GPTD_BFTM0, &bftm0Config);
+    gptStartContinuous(&GPTD_BFTM0, 1);
 
-  // Setup Animation Timer
-  gptStart(&GPTD_BFTM1, &lightAnimationConfig);
-  gptStartContinuous(&GPTD_BFTM1, 1);
+    // Setup Animation Timer
+    // gptStart(&GPTD_BFTM1, &lightAnimationConfig);
+    // gptStartContinuous(&GPTD_BFTM1, 1);
 
-  chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
+    chThdCreateStatic(waThread1, sizeof(waThread1), NORMALPRIO, Thread1, NULL);
 
-  while (true) {
-
-  }
+    while (true) {
+        // Low priority thread
+    }
 }
--- source/light_utils.c
+++ source/light_utils.c
@@ -13,11 +13,6 @@
 /*
     Static Declarations
 */
-
-// An array of basic colors used accross different lighting profiles
-static const uint32_t colorPalette[] = {0xF0000, 0xF0F00, 0x00F00, 0x00F0F, 0x0000F, 0xF000F, 0x50F0F};
-
-
 // Array with Modifier keys IDs (Esc, Tab, Ctrl, Enter etc) 
 static const uint8_t modKeyIDs[] = {0, 13, 14, 28, 40, 41, 42, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69};
 
@@ -26,28 +21,28 @@ static const uint8_t modKeyIDs[] = {0, 13, 14, 28, 40, 41, 42, 54, 55, 56, 57, 5
 */
 
 // Set all keys lighting to a specific color
-void setAllKeysColor(led_t* ledColors, uint32_t color){
+void setAllKeysColor(led_t* currentKeyLedColors, uint32_t color){
     const uint8_t red = (color >> 16) & 0xFF;
     const uint8_t green = (color >> 8) & 0xFF;
     const uint8_t blue = color & 0xFF;
 
     for (uint16_t i=0; i<NUM_COLUMN * NUM_ROW; ++i){
-        ledColors[i].red = red;
-        ledColors[i].green = green;
-        ledColors[i].blue = blue;
+        currentKeyLedColors[i].red = red;
+        currentKeyLedColors[i].green = green;
+        currentKeyLedColors[i].blue = blue;
     }
 }
 
 // Set modifier keys lighting to a specific color
-void setModKeysColor(led_t* ledColors, uint32_t color){
+void setModKeysColor(led_t* currentKeyLedColors, uint32_t color){
     const uint8_t red = (color >> 16) & 0xFF;
     const uint8_t green = (color >> 8) & 0xFF;
     const uint8_t blue = color & 0xFF;
     
     for (uint16_t i=0; i<LEN(modKeyIDs); ++i){
-        ledColors[modKeyIDs[i]].red = red;
-        ledColors[modKeyIDs[i]].green = green;
-        ledColors[modKeyIDs[i]].blue = blue;
+        currentKeyLedColors[modKeyIDs[i]].red = red;
+        currentKeyLedColors[modKeyIDs[i]].green = green;
+        currentKeyLedColors[modKeyIDs[i]].blue = blue;
     }
 }
 

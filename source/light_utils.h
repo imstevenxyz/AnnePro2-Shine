b88978d5d8e48756d9a80a0652770c3d114ec358
Steven Impens <s.impens.dev@gmail.com>
2020-08-03 16:11:29 +0200

    Removed unused struct and signatures

--- source/light_utils.h
+++ source/light_utils.h
@@ -11,10 +11,6 @@ typedef struct {
     uint8_t red, green, blue;
 } led_t;
 
-typedef struct {
-    double hue, saturation, brightness;
-} hsb_t;
-
 /*
     Function Signatures
 */
@@ -24,10 +20,5 @@ void setKeyColor(led_t *key, uint32_t color);
 void upBrightness(void);
 void downBrightness(void);
 led_t rgbWithBrtModifier(uint32_t color);
-//hsb_t hexToHsb(uint32_t color_hex);
-//led_t hsbToRgb(hsb_t color_hsb);
-//double max(double a, double b, double c);
-//double min(double a, double b, double c);
-//double dmod(double x, double y);
 
 #endif
